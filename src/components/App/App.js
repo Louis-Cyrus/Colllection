e268b7c80de0f5c4f17253722570303275b745cf
Louis-Cyrus Sanjabi <87710724+Louis-Cyrus@users.noreply.github.com>
2023-07-26 17:33:03 +0200

    change repository"

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+import styles from './App.module.css';
+
 export default function App() {
     return (
         <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import Image from 'next/image';
 import Head from 'next/head';
-import App from '../components/App';
+import App from '../components/App/App';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Jammming, create your playlist !</title>
-        <meta name="description" content="Jammming, create your playlist !" />
+        <title>Colllection, ma collection de films et livres</title>
+        <meta name="description" content="Colllection, ma collection de films et livres." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <div>
